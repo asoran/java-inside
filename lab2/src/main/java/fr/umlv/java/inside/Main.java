bad74b19efe8d23647dcdc504d2dccba1dcfaf12
asoran <asoran@condorcet93.fr>
2019-10-01 09:58:27 +0200

    better version of question 2

--- lab2/src/main/java/fr/umlv/java/inside/Main.java
+++ lab2/src/main/java/fr/umlv/java/inside/Main.java
@@ -1,5 +1,7 @@
 package fr.umlv.java.inside;
 
+import static java.util.stream.Collectors.joining;
+
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.stream.Collectors;
@@ -11,6 +13,10 @@ public class Main {
 		return Character.toLowerCase(name.charAt(3)) + name.substring(4);
 	}
 
+	private static Boolean isGeter(String name) {
+		return name.startsWith("get");
+	}
+
 	public static String toJSON(Object o) {
 
 		var clazz = o.getClass();
@@ -18,11 +24,11 @@ public class Main {
 
 		var nameValueMap = methods
 			.map(m -> m.getName())
-			.filter(m -> m.startsWith("get"))
+			.filter(Main::isGeter)
 			.map(Main::propertyName)
-			.collect(Collectors.joining(", "));
+			.collect(joining(", ", "{", "}"));
 
-		
+
 		return nameValueMap;
 	}
 	
