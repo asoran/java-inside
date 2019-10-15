f347a677ae58b4bf5b99fec63f447a9b951620a2
asoran <asoran@condorcet93.fr>
2019-10-15 09:38:18 +0200

    created lab4 and tests are written

---
+++ lab4/src/main/java/fr/umlv/java/inside/Logger.java
@@ -0,0 +1,34 @@
+package fr.umlv.java.inside;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.reflect.UndeclaredThrowableException;
+import java.util.function.Consumer;
+
+public interface Logger {
+	public void log(String message);
+
+	public static Logger of(Class<?> declaringClass, Consumer<? super String> consumer) {
+		var mh = createLoggingMethodHandle(declaringClass, consumer);
+		return new Logger() {
+			@Override
+			public void log(String message) {
+				try {
+					mh.invokeExact(message);
+				} catch (Throwable t) {
+					if (t instanceof RuntimeException) {
+						throw (RuntimeException) t;
+					}
+					if (t instanceof Error) {
+						throw (Error) t;
+					}
+					throw new UndeclaredThrowableException(t);
+				}
+			}
+		};
+	}
+
+	private static MethodHandle createLoggingMethodHandle(Class<?> declaringClass, Consumer<? super String> consumer) {
+		// TODO
+		return null;
+	}
+}
---
+++ lab4/src/test/java/fr/umlv/java/inside/LoggerTests.java
@@ -0,0 +1,36 @@
+package fr.umlv.java.inside;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class LoggerTests {
+	
+	@Test
+	@Tag("Question2")
+	public void LoggerRequireNotNullClass() {
+		assertThrows(NullPointerException.class, () -> Logger.of(null, System.err::println));
+	}
+
+	@Test
+	@Tag("Question2")
+	public void LoggerRequireNotNullConsumer() {
+		assertThrows(NullPointerException.class, () -> Logger.of(LoggerTests.class, null));
+	}
+
+	private final static StringBuilder sb = new StringBuilder();
+	private final static Logger logger = Logger.of(LoggerTests.class, sb::append);
+
+	@Test
+	@Tag("Question2")
+	public void LoggerLogsGoodLol() {
+		logger.log("Salut");
+		assertEquals("Salut", sb.toString());
+	}
+
+
+}
