b5349b68371d12b20a232936fdc0e4ff52e545a7
asoran <asoran@condorcet93.fr>
2019-10-15 11:03:45 +0200

    Added JMH dependency and benchmark test

---
+++ lab4/src/test/java/fr/umlv/java/inside/LoggerBenchMark.java
@@ -0,0 +1,35 @@
+package fr.umlv.java.inside;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+@Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Fork(3)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@State(Scope.Benchmark)
+public class LoggerBenchMark {
+	@Benchmark
+	public void no_op() {
+		// empty
+	}
+
+	@Benchmark
+	public void simple_logger() {
+		// TODO
+	}
+}
