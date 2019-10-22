2f28f6790aaea3a43b0c6b724e386ac3dd8a36cb
asoran <altan.soran@gmail.com>
2019-10-22 10:20:39 +0200

    forgot to add StringSwitchExample.java

--- lab5/src/main/java/fr/umlv/java/inside/StringSwitchExample.java
+++ lab5/src/main/java/fr/umlv/java/inside/StringSwitchExample.java
@@ -1,5 +1,24 @@
 package fr.umlv.java.inside;
 
+import java.lang.invoke.MethodHandle;
+import java.util.Objects;
+
 public class StringSwitchExample {
 
+	public static int stringSwitch(String string) {
+		Objects.requireNonNull(string);
+
+		switch(string) {
+			case "foo":
+				return 0;
+			case "bar":
+				return 1;
+			case "bazz":
+				return 2;
+			default:
+				return -1;
+		}
+	}
+
+	
 }
