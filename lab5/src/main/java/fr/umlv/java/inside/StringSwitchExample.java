06bedfcf905c851e565d822f122cf0ace441d085
asoran <altan.soran@gmail.com>
2019-10-22 09:22:03 +0200

    test for StringSwitchExample written

---
+++ lab5/src/main/java/fr/umlv/java/inside/StringSwitchExample.java
@@ -0,0 +1,5 @@
+package fr.umlv.java.inside;
+
+public class StringSwitchExample {
+
+}
---
+++ lab5/src/test/java/fr/umlv/java/inside/StringSwitchExampleTests.java
@@ -0,0 +1,31 @@
+package fr.umlv.java.inside;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class StringSwitchExampleTests {
+
+	@Test
+	@Tag("Question3")
+	public void stringSwitchWorksCorrectly() {
+		assertAll(
+			assertEquals(0, StringSwitchExample.stringSwitch("foo"))
+			assertEquals(1, StringSwitchExample.stringSwitch("bar"))
+			assertEquals(2, StringSwitchExample.stringSwitch("bazz"))
+			assertEquals(-1, StringSwitchExample.stringSwitch("other"))
+		);
+	}
+	
+	@Test
+	@Tag("Question3")
+	public void stringSwitchThrowsNPEWhenNullGiven() {
+		assertThrows(NullPointerException.class,
+			() -> StringSwitchExample.stringSwitch(null));
+	}
+
+	
+}
