db80460797be3b2470992d99350ab4a1056c57bc
asoran <altan.soran@gmail.com>
2019-11-02 12:10:19 +0100

    change Example1 to use our custome Scheduler

--- lab6/src/main/java/fr/umlv/java/inside/Example1.java
+++ lab6/src/main/java/fr/umlv/java/inside/Example1.java
@@ -1,46 +1,39 @@
 package fr.umlv.java.inside;
 
-import java.util.ArrayDeque;
-import java.util.List;
-
 public class Example1 {
 
 	public static void main(String[] args) {
-		var scope = new ContinuationScope("scope");
+		var scope = new ContinuationScope("Scheduler");
+		Scheduler sc = new Scheduler(Scheduler.POLITIC.RANDOM);
 
 		var contiuation1 = new Continuation(scope, () -> {
 			System.out.println("start 1");
-			Continuation.yield(scope);
+			sc.enqueue(scope);
 			System.out.println("middle 1");
-			Continuation.yield(scope);
+			sc.enqueue(scope);
 			System.out.println("end 1");
 		});
 
 		var contiuation2 = new Continuation(scope, () -> {
 			System.out.println("start 2");
-			Continuation.yield(scope);
+			sc.enqueue(scope);
 			System.out.println("middle 2");
-			Continuation.yield(scope);
+			sc.enqueue(scope);
 			System.out.println("end 2");
 		});
 
 		var contiuation3 = new Continuation(scope, () -> {
 			System.out.println("start 3");
-			Continuation.yield(scope);
+			sc.enqueue(scope);
 			System.out.println("middle 3");
-			Continuation.yield(scope);
+			sc.enqueue(scope);
 			System.out.println("end 3");
 		});
 
-		var list = List.of(contiuation1, contiuation2, contiuation3);
-
-		var deck = new ArrayDeque<>(list);
-		while(!deck.isEmpty()) {
-			var e = deck.poll();
-			e.run();
-			if(!e.isDone())
-				deck.offer(e);
-		}
+		contiuation1.run();
+		contiuation2.run();
+		contiuation3.run();
+		sc.runLoop();
 
 	}
 
